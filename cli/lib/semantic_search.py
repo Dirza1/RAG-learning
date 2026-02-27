a3ea00fec6d6ba54d12741ef3533924ae041bca9
jasper.olthof <jasper.olthof@thermofisher.com>
2026-02-27 11:20:13 +0100

    started on simantic chunk embedding

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -58,6 +58,16 @@ class SemanticSearch:
         return final_return
 
         
+class ChunkedSemanticSearch(SemanticSearch):
+    def __init__(self, model_name = "all-MiniLM-L6-v2") -> None:
+        super().__init__(model_name)
+        self.chunk_embeddings = None
+        self.chunk_metadata = None
+
+    def build_chunk_embeddings(self,documents):
+        self.documents = documents
+        for document in documents:
+            self.document_map[document['id']] = document
 
 
 def verify_model()->None:
